ffe234e04efde5c3c998a6d136f5d6818f87d87b
Qi-Month <dy757410@163.com>
2025-01-17 01:22:25 +0800

    Adjust the minority recipes Add optional recipes example

--- kubejs/startup_scripts/src/modify/sound.js
+++ kubejs/startup_scripts/src/modify/sound.js
@@ -14,16 +14,15 @@ BlockEvents.modification((event) => {
 	})
 
 	let lanternSoundType = [
+		// SteamPowered
 		"steampowered:bronze_boiler",
 		"steampowered:bronze_burner",
 		"steampowered:bronze_steam_engine",
 		"steampowered:bronze_flywheel",
-
 		"steampowered:cast_iron_boiler",
 		"steampowered:cast_iron_burner",
 		"steampowered:cast_iron_steam_engine",
 		"steampowered:cast_iron_flywheel",
-
 		"steampowered:steel_boiler",
 		"steampowered:steel_burner",
 		"steampowered:steel_steam_engine",
