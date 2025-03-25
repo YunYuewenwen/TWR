31bebb84d9dcf294534ba9c2a5a1301108f6a456
yuesha-yc <kevinwangyichen@gmail.com>
2025-03-24 23:38:43 -0700

    Ice chips

--- kubejs/server_scripts/src/loot_tables/loot_modifiers.js
+++ kubejs/server_scripts/src/loot_tables/loot_modifiers.js
@@ -1,8 +1,31 @@
 LootJS.modifiers((event) => {
+	// vines and sticks
 	event.addBlockLootModifier("#minecraft:leaves")
 		.matchMainHand("#minecraft:swords")
-		.randomChance(0.7)
+		.randomChance(0.3)
 		.addLoot("minecraft:vine")
 		.randomChance(0.3)
 		.addLoot("minecraft:stick");
+	// ice chips
+	event.addBlockLootModifier("frostedheart:thin_ice").removeLoot("frostedheart:ice_chip");
+	event.addBlockLootModifier("frostedheart:thin_ice")
+		.randomChance(1.0)
+		.addLoot("frostedheart:ice_chip x1")
+		.randomChance(0.3)
+		.addLoot("frostedheart:ice_chip x2")
+	event.addBlockLootModifier("minecraft:ice")
+		.randomChance(1.0)
+		.addLoot("frostedheart:ice_chip x1")
+		.randomChance(0.5)
+		.addLoot("frostedheart:ice_chip x2")
+	event.addBlockLootModifier("minecraft:packed_ice")
+		.randomChance(1.0)
+		.addLoot("frostedheart:ice_chip x1")
+		.randomChance(0.7)
+		.addLoot("frostedheart:ice_chip x2")
+	event.addBlockLootModifier("minecraft:blue_ice")
+		.randomChance(1.0)
+		.addLoot("frostedheart:ice_chip x2")
+		.randomChance(0.9)
+		.addLoot("frostedheart:ice_chip x2")
 });
