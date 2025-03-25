07c3968697578ac1dae5b417a728aa227f535bb8
yuesha-yc <kevinwangyichen@gmail.com>
2025-03-25 04:58:16 -0700

    Bronze

--- kubejs/server_scripts/src/recipes_types/charcoal_pit/pottery.js
+++ kubejs/server_scripts/src/recipes_types/charcoal_pit/pottery.js
@@ -4,6 +4,11 @@ ServerEvents.recipes((event) => {
     charcoal_pit.pottery("minecraft:stone", "#forge:cobblestone")
     charcoal_pit.pottery("minecraft:stone", "#forge:stone")
 
+    charcoal_pit.orekiln("frostedheart:bronze_ingot", ["#forge:ingots/copper", "#forge:ingots/copper", "#forge:ingots/copper", "#forge:ingots/tin"], 4)
+    charcoal_pit.orekiln("frostedheart:bronze_ingot", ["#forge:raw_materials/copper", "#forge:raw_materials/copper", "#forge:raw_materials/copper", "#forge:raw_materials/tin"], 4)
+    charcoal_pit.orekiln("minecraft:copper_ingot", ["frostedheart:condensed_ball_copper_ore", "frostedheart:condensed_ball_copper_ore", "frostedheart:condensed_ball_copper_ore", "frostedheart:condensed_ball_copper_ore"], 1)
+    charcoal_pit.orekiln("frostedheart:tin_ingot", ["frostedheart:condensed_ball_tin_ore", "frostedheart:condensed_ball_tin_ore", "frostedheart:condensed_ball_tin_ore", "frostedheart:condensed_ball_tin_ore"], 1)
+
     charcoal_pit.orekiln("minecraft:copper_ingot", ["#forge:raw_materials/copper"], 1)
     charcoal_pit.orekiln("minecraft:copper_ingot", ["#forge:ingots/rusted_copper"], 1)
     charcoal_pit.orekiln("frostedheart:tin_ingot", ["#forge:raw_materials/tin"], 1)
