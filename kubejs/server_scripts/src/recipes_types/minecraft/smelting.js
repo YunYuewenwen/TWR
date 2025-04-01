92ccefb254f86fb8cba9370d16137125fce22cee
dasb <936336596@qq.com>
2025-04-01 15:50:49 +0800

    fix misc

--- kubejs/server_scripts/src/recipes_types/minecraft/smelting.js
+++ kubejs/server_scripts/src/recipes_types/minecraft/smelting.js
@@ -4,16 +4,8 @@ ServerEvents.recipes((event) => {
     minecraft.smelting("minecraft:terracotta", "minecraft:clay")
 
     let recipes = [
-        {output: 'frostedheart:alumina_dust', input: 'frostedheart:aluminium_hydroxide_dust'},
         {output: 'minecraft:black_dye', input: '#kubejs:coal_dust'},
         {output: 'minecraft:black_dye', input: 'kubejs:charcoal_dust'},
-        {output: 'kubejs:coal_briquette', input: 'kubejs:wet_coal_briquette'},
-        {output: 'kubejs:coal_briquette', input: 'kubejs:creosoted_coal_briquette'},
-        {output: 'kubejs:charcoal_briquette', input: 'kubejs:creosoted_charcoal_briquette'},
-        {output: 'kubejs:coal_briquette', input: 'kubejs:bound_coal_briquette'},
-        {output: 'kubejs:charcoal_briquette', input: 'kubejs:bound_charcoal_briquette'},
-        {output: 'kubejs:sawdust_briquette', input: 'kubejs:creosoted_sawdust_briquette'},
-        {output: 'kubejs:straw_briquette', input: 'kubejs:wet_straw_briquette'},
         {output: '#forge:ingots/tin', input: 'frostedheart:gray_tin_ingot'},
         {output: '4x minecraft:red_dye', input: 'frostedheart:lead_oxide_dust'},
         {output: 'create:zinc_ingot', input: '#forge:dusts/zinc'},
@@ -29,7 +21,7 @@ ServerEvents.recipes((event) => {
         {output: 'frostedheart:peat_block', input: 'frostedheart:peat_permafrost'},
         {output: 'frostedheart:bauxite_block', input: 'frostedheart:bauxite_permafrost'},
         {output: 'frostedheart:kaolin_block', input: 'frostedheart:kaolin_permafrost'},
-        {output: 'minecraft:paper', input: 'frostedheart:pulp'},
+        {output: 'minecraft:paper', input: 'frostedheart:pulp'}
     ]
     recipes.forEach((recipe) => {
         minecraft.smelting(recipe.output, recipe.input)
--- kubejs/server_scripts/src/recipes_types/minecraft/smoking.js
+++ kubejs/server_scripts/src/recipes_types/minecraft/smoking.js
@@ -2,4 +2,20 @@ ServerEvents.recipes((event) => {
     let {minecraft} = event.recipes
     minecraft.smoking(Item.of('frostedheart:wooden_cup_drink', '{Fluid:{FluidName:"caupona:nail_soup",Amount:250}}'), Item.of('frostedheart:wooden_cup_drink', '{Fluid:{FluidName:"minecraft:water",Amount:250}}'))
     minecraft.smoking('frostedheart:rye_bread', Item.of('frostedheart:raw_rye_bread'))
+
+    minecraft.smelting("minecraft:terracotta", "minecraft:clay")
+
+    let recipes = [
+        {output: 'frostedheart:alumina_dust', input: 'frostedheart:aluminium_hydroxide_dust'},
+        {output: 'kubejs:coal_briquette', input: 'kubejs:wet_coal_briquette'},
+        {output: 'kubejs:coal_briquette', input: 'kubejs:creosoted_coal_briquette'},
+        {output: 'kubejs:charcoal_briquette', input: 'kubejs:creosoted_charcoal_briquette'},
+        {output: 'kubejs:coal_briquette', input: 'kubejs:bound_coal_briquette'},
+        {output: 'kubejs:charcoal_briquette', input: 'kubejs:bound_charcoal_briquette'},
+        {output: 'kubejs:sawdust_briquette', input: 'kubejs:creosoted_sawdust_briquette'},
+        {output: 'kubejs:straw_briquette', input: 'kubejs:wet_straw_briquette'}
+    ]
+    recipes.forEach((recipe) => {
+        minecraft.smoking(recipe.output, recipe.input)
+    })
 })
