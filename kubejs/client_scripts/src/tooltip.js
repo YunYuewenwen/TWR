b09cb77a224f69cc87cbcf5f4e7d6ee0b1b10153
yuesha-yc <kevinwangyichen@gmail.com>
2025-04-03 22:28:01 -0700

    Tooltips

--- kubejs/client_scripts/src/tooltip.js
+++ kubejs/client_scripts/src/tooltip.js
@@ -7,38 +7,6 @@ ItemEvents.tooltip((event) => {
         {
             items: ["minecraft:blast_furnace", "minecraft:furnace"],
             key: ["replaced_recipe"]
-        },
-        {
-            items: ["minecraft:vine"],
-            key: ["snowy_vine"]
-        },
-        {
-            items: ["frostedheart:makeshift_knife"],
-            key: ["flint_knife"]
-        },
-        {
-            items: ["stone_age:flint_knife"],
-            key: ["flint_knife_2"]
-        },
-        {
-            items: ["stone_age:tanning_rack"],
-            key: ["tanning_rack"]
-        },
-        {
-            items: ["kubejs:activated_carbon"],
-            key: ["activated_carbon"]
-        },
-        {
-            items: ["minecraft:campfire"],
-            key: ["campfire"]
-        },
-        {
-            items: ["frostedheart:straw_lining", "frostedheart:kelp_lining", "frostedheart:gambeson", "frostedheart:buff_coat"],
-            key: ["lining"]
-        },
-        {
-            items: ["create:blaze_burner"],
-            key: ["blaze_burner"]
         }
     ]
     addTooltip.forEach((recipe) => {
